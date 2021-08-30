31e300c09284de6bdd43ef91ad642b8335a16505
Paul0620 <dlrhkdghsp@gmail.com>
2021-08-30 23:04:15 +0900

    Update index.html, app.js

---
+++ README.md
@@ -0,0 +1,11 @@
+[1. 과제 - 핸들러 이용하여 화면동작](https://codesandbox.io/s/day-three-blueprint-forked-ijvgy?file=/src/index.js)
+
+- 슈퍼핸들러 안에 여러 핸들러를 만들어 그 동작에 따라 글내용, 글자색을 바꾸기 정답(https://codesandbox.io/s/day-three-solution-3n53e)
+
+[2. 과제 - if, else를 이용한 event처리](https://codesandbox.io/s/empty-blueprint-forked-gi6zt)
+
+- window.innerWidth를 이용하여 인터넷 창의 사이즈를 변수에 담아 넓이에 따라 if, else문을 사용하여 색깔변화 이때 색깔변화는 classList를 이용해 html에 추가 제거하여 css를 적용시킨다 정답(같은 링크에 주석처리로 넣어놓음)
+
+[3. 과제 - 랜덤 숫자 맞추기](https://codesandbox.io/s/empty-blueprint-forked-05ttg)
+
+- form을 이용하여 값을 랜덤범위의 최대값, 추측값을 보내 결과값을 반환
--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const age = parseInt(prompt("How old are you?"));
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-if (isNaN(age)){
-  console.log("Please write a number.")
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-  console.log("Thank you for wirting your age.")
+  paintGreetings(savedUsername);
 }
